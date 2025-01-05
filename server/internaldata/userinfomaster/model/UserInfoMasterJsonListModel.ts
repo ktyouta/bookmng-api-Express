e457928bed50b6a164fa4b854e9716e176f3e4c1
ktyouta <ktyouta0506@gmail.com>
2025-01-05 17:40:57 +0900

    ユーザー登録処理のリファクタリング中

---
+++ server/internaldata/userinfomaster/model/UserInfoMasterJsonListModel.ts
@@ -0,0 +1,66 @@
+import { JsonFileOperation } from "../../../util/service/JsonFileOperation";
+import { USER_INFO_MASTER_FILE_PATH } from "../const/UserInfoMasterConst";
+import { UserInfoMasterJsonModelType } from "./UserInfoMasterJsonModelType";
+import { UserInfoMasterListModel } from "./UserInfoMasterListModel";
+import { UserInfoMasterModel } from "./UserInfoMasterModel";
+
+export class UserInfoMasterJsonListModel {
+
+    private readonly _userInfoMasterJsonList: UserInfoMasterJsonModelType[];
+
+
+    constructor(userInfoMasterListModel: UserInfoMasterListModel) {
+
+        // jsonファイル登録用の型に変換する
+        const jsonUserInfoMasterListModel = userInfoMasterListModel.userInfoMasterModelList.map((e: UserInfoMasterModel) => {
+            return this.parseJsonUserInfoMaster(e);
+        });
+
+        this._userInfoMasterJsonList = jsonUserInfoMasterListModel;
+    }
+
+
+    /**
+     * getter
+     */
+    public get userInfoMasterJsonList() {
+        return this._userInfoMasterJsonList
+    }
+
+
+    /**
+     * UserInfoMasterModelからjson形式に変換する
+     * @param userInfoMaster 
+     * @returns 
+     */
+    private parseJsonUserInfoMaster(userInfoMaster: UserInfoMasterModel): UserInfoMasterJsonModelType {
+
+        // jsonファイル登録用の型に変換する
+        const jsonUserInfoMaster: UserInfoMasterJsonModelType = {
+            userId: userInfoMaster.userId,
+            userName: userInfoMaster.userName,
+            userBirthDay: userInfoMaster.userBirthDay,
+            createDate: userInfoMaster.createDate,
+            updateDate: userInfoMaster.updateDate,
+            deleteFlg: userInfoMaster.deleteFlg,
+        };
+
+        return jsonUserInfoMaster;
+    }
+
+
+    /**
+     * ユーザーマスタファイルにデータを書き込む
+     */
+    public overWriteUserInfoMaster() {
+
+        try {
+
+            JsonFileOperation.overWriteJsonFileData(USER_INFO_MASTER_FILE_PATH, this._userInfoMasterJsonList);
+        } catch (err) {
+
+            throw Error(`ユーザーマスタファイルのデータ書き込み処理中にエラーが発生しました。ERROR:${err}`);
+        }
+    }
+
+}
---
+++ server/internaldata/userinfomaster/model/UserInfoMasterListModel.ts
@@ -0,0 +1,152 @@
+import { FLG } from "../../../util/const/CommonConst";
+import { JsonFileOperation } from "../../../util/service/JsonFileOperation";
+import { USER_INFO_MASTER_FILE_PATH } from "../const/UserInfoMasterConst";
+import { CreateDateModel } from "./CreateDateModel";
+import { DeleteFlgModel } from "./DeleteFlgModel";
+import { UpdateDateModel } from "./UpdateDateModel";
+import { UserBirthdayModel } from "./UserBirthDayModel";
+import { UserIdModel } from "./UserIdModel";
+import { UserInfoMasterCreateModel } from "./UserInfoMasterCreateModel";
+import { UserInfoMasterJsonListModel } from "./UserInfoMasterJsonListModel";
+import { UserInfoMasterJsonModelType } from "./UserInfoMasterJsonModelType";
+import { UserInfoMasterModel } from "./UserInfoMasterModel";
+import { UserNameModel } from "./UserNameModel";
+
+
+export class UserInfoMasterListModel {
+
+    private readonly _userInfoMasterModelList: UserInfoMasterModel[];
+
+    private constructor(UserInfoMasterModelList: UserInfoMasterModel[]) {
+
+        this._userInfoMasterModelList = UserInfoMasterModelList;
+    }
+
+
+    /**
+     * getter
+     */
+    public get userInfoMasterModelList() {
+        return this._userInfoMasterModelList;
+    }
+
+
+    /**
+     * ユーザーマスターファイルからデータを取得する
+     * @returns 
+     */
+    public static getUserInfoMasterList() {
+
+        // ユーザーマスタファイルからデータを取得
+        const jsonUserInfoMasterList: UserInfoMasterJsonModelType[] = JsonFileOperation.getFileObj(USER_INFO_MASTER_FILE_PATH);
+
+        // json形式からUserInfoMasterModelに変換する
+        const parsedUserInfoMasterList = jsonUserInfoMasterList.map((e: UserInfoMasterJsonModelType) => {
+            return this.parseUserInfoMaster(e);
+        });
+
+        return new UserInfoMasterListModel(parsedUserInfoMasterList);
+    }
+
+
+    /**
+     * json形式からUserInfoMasterModelに変換する
+     * @param jsonUserInfoMaster 
+     * @returns 
+     */
+    private static parseUserInfoMaster(jsonUserInfoMaster: UserInfoMasterJsonModelType): UserInfoMasterModel {
+
+        const userIdModel = UserIdModel.reConstruct(jsonUserInfoMaster.userId);
+        const userNameModel = new UserNameModel(jsonUserInfoMaster.userName);
+        const userBirthdayModel = new UserBirthdayModel(jsonUserInfoMaster.userBirthDay);
+        const createDate = CreateDateModel.reConstruct(jsonUserInfoMaster.createDate, `ユーザーマスタ`);
+        const updateDate = UpdateDateModel.reConstruct(jsonUserInfoMaster.updateDate, `ユーザーマスタ`);
+        const deleteFlgModel = new DeleteFlgModel(jsonUserInfoMaster.deleteFlg);
+
+        return new UserInfoMasterModel(
+            userIdModel,
+            userNameModel,
+            userBirthdayModel,
+            createDate,
+            updateDate,
+            deleteFlgModel
+        );
+    }
+
+
+    /**
+     * 未削除のユーザーデータを取得
+     * @param userInfoMasterList 
+     * @returns 
+     */
+    public getActiveUserInfoMaster(userInfoMasterListModel: UserInfoMasterListModel) {
+
+        // 未削除のユーザーを取得
+        const activeUserInfoMasterList = userInfoMasterListModel._userInfoMasterModelList.filter((e: UserInfoMasterModel) => {
+
+            return e.deleteFlg !== FLG.ON;
+        });
+
+        return new UserInfoMasterListModel(activeUserInfoMasterList);
+    }
+
+
+    /**
+     * ユーザーマスタに対する書き込み用データの作成
+     * @param userInfoMasterList 
+     * @param userInfoMasterCreateModel 
+     * @returns 
+     */
+    public createUserInfoMasterWriteData(
+        userInfoMasterListModel: UserInfoMasterListModel,
+        userInfoMasterCreateModel: UserInfoMasterCreateModel): UserInfoMasterListModel {
+
+        // UserInfoMasterCreateModel→UserInfoMasterModelに変換する
+        const createUserInfoMaster: UserInfoMasterModel = this.parseCreateUserInfoMaster(userInfoMasterCreateModel);
+
+        // ユーザーを追加する
+        const createUserInfoMasterList = [...userInfoMasterListModel._userInfoMasterModelList, createUserInfoMaster];
+
+        return new UserInfoMasterListModel(createUserInfoMasterList);
+    }
+
+
+    /**
+     * UserInfoMasterCreateModelからUserInfoMasterModelに変換する
+     * @param userInfoMasterCreateModel 
+     * @returns 
+     */
+    private parseCreateUserInfoMaster(userInfoMasterCreateModel: UserInfoMasterCreateModel): UserInfoMasterModel {
+
+        const userIdModel = userInfoMasterCreateModel.userIdModel;
+        const userNameModel = userInfoMasterCreateModel.userNameModel;
+        const userBirthdayModel = userInfoMasterCreateModel.userBirthDayModel;
+        const createDate = userInfoMasterCreateModel.createDateModel;
+        const updateDate = userInfoMasterCreateModel.updateDateModel;
+        const deleteFlgModel = userInfoMasterCreateModel.deleteFlgModel;
+
+        return new UserInfoMasterModel(
+            userIdModel,
+            userNameModel,
+            userBirthdayModel,
+            createDate,
+            updateDate,
+            deleteFlgModel
+        );
+    }
+
+
+    /**
+     * ユーザー名の重複チェック
+     * @param userNameModel 
+     */
+    public checkUserNameExists(userNameModel: UserNameModel): boolean {
+
+        const activeUserMaster = this._userInfoMasterModelList.find((e: UserInfoMasterModel) => {
+            return e.userNameModel.checkUsernameDuplicate(userNameModel);
+        });
+
+        return !!activeUserMaster;
+    }
+
+}
